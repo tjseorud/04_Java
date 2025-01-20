44df8cdb7e22684ebd056d53cbcc7c8a5e36aaa8
tjseorud <35774943+tjseorud@users.noreply.github.com>
2025-01-20 11:27:13 +0900

    Java Workspace

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,28 @@
+// JS와 주석 동일
+/* Run 버튼 클릭시 [Ctrl + F11]
+ * 1) 자동으로 컴파일 수행(javac ???.java)
+ * 	-> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행(java class파일명)
+ * */
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("이클립스 실행");
+		System.out.println("히히모카");
+		System.out.println("으어으어으어");
+		/* Java API : Java에서 제공해주는 코드의 사용법이 설명 되어있는 문서
+		 * 
+		 * DOC 확인방법
+		 * 1) Java 21 api 검색
+		 * 2) 궁금한 코드 클릭 -> [Shift + F2]
+		 * 
+		 * Git push 주의사항
+		 * .matadata 폴더 : eclipse 설정 모음 폴더
+		 * .classpath : class 경로 인식 방법 설정
+         * bin : 컴파일된 파일(바이트 코드) 모인 폴더
+         * .class : 컴파일된 파일(바이트 코드)
+		 * */
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,9 @@
+public class JavaTest{
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		for(int i=0;i<10;i++){
+			System.out.println("\n");
+			System.out.println("집 가고싶다");
+		}	
+	}
+}
